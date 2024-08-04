12115b2232df7349917b5d6c8987d33d7eba7a18
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-08-04 21:16:41 +0300

    tailwind setup

---
+++ client/nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+        link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+        title: 'Academia'
+    }
+},
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
---
+++ client/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    // customized breakpoints //
+    screens: {
+      smallscreen: "300px",
+
+      mobile: "600px",
+
+      tablet: "760px",
+      // => @media (min-width: 760px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      lgdesk: "1600px",
+    },
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar") ({ nocompatible: true })],
+};
