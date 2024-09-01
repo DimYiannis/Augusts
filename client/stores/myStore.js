a032222050f63b50df47d0a8c928e71597225fb5
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-09-01 17:34:26 +0300

    Update myStore.js

--- client/stores/myStore.js
+++ client/stores/myStore.js
@@ -1,7 +1,7 @@
 // stores/myStore.js
 import { defineStore } from "pinia";
-import axios from 'axios';
-import { piniaPersistConfig } from './piniaPersistConfig';
+import axios from "axios";
+import { piniaPersistConfig } from "./piniaPersistConfig";
 
 export const useUserStore = defineStore("user", {
   state: () => ({
@@ -11,13 +11,15 @@ export const useUserStore = defineStore("user", {
   actions: {
     async login() {
       try {
-        this.isLoggedIn = true;
-        // Fetch user data after login
-        const response = await axios.get('http://localhost:5000/api/v1/users/showUser');
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/users/showUser",  { withCredentials: true }
+        );
         console.log(response.data);
-        this.user = response.data;  // Store user data in the state
+        this.user = response.data.user; // Store user data in the state
+
+        this.isLoggedIn = true;
       } catch (error) {
-        console.error('Error fetching user information:', error);
+        console.error("Error fetching user information:", error);
       }
     },
     logout() {
@@ -25,7 +27,7 @@ export const useUserStore = defineStore("user", {
       this.user = null;
     },
   },
-  persist: piniaPersistConfig('userStore'),
+  persist: piniaPersistConfig("userStore"),
 });
 
 export const useModalsStore = defineStore("modal", {
@@ -36,6 +38,10 @@ export const useModalsStore = defineStore("modal", {
     showFav: false,
     showCart: false,
     showTooltip: false,
+    favItems: [],
+    isAddingToFav: false,
+    isRemovingFromFav: false,
+    isFetchingFav: false,
   }),
   actions: {
     toggleLogin() {
@@ -49,37 +55,189 @@ export const useModalsStore = defineStore("modal", {
     toggleLogout() {
       this.showLogout = !this.showLogout;
     },
-    
-    addToCart(item) {
-      // Logic to add to cart
-      this.cartItems.push(item);
+    toggleFav() {
+      this.showFav = !this.showFav;
     },
-    removeFromCart(item) {
-      // Logic to remove from cart
-      const index = this.cartItems.findIndex(
-        (cartItem) => cartItem.id === item.id
-      );
-      if (index !== -1) {
-        this.cartItems.splice(index, 1);
+    toggleCart() {
+      this.showCart = !this.showCart;
+    },
+
+    async fetchFavItems() {
+      this.isFetchingFav = true;
+      try {
+        const response = await axios.get("http://localhost:5000/api/v1/likes", { withCredentials: true });
+
+        console.log("Favorite items fetched successfully:", response.data);
+
+        this.favItems = Array.isArray(response.data.items) ? response.data.items : [];
+      } catch (error) {
+        console.error("Error fetching favorite items:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+        }
+      } finally {
+        this.isFetchingFav = false;
       }
     },
-    addToFav(item) {
-      // Logic to add to favorites
-      this.favItems.push(item);
+
+    async addToFav(item) {
+      this.isAddingToFav = true;
+      try {
+        const response = await axios.post("http://localhost:5000/api/v1/likes", item, { withCredentials: true });
+
+        console.log("Item added to favorites successfully:", response.data);
+
+        const index = this.favItems.findIndex(
+          (favItem) => favItem.id === item.id && favItem.size === item.size
+        );
+
+        if (index === -1) {
+          this.favItems.push({ ...item });
+        }
+      } catch (error) {
+        console.error("Error adding item to favorites:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+        }
+      } finally {
+        this.isAddingToFav = false;
+      }
     },
-    removeFromFav(item) {
-      // Logic to remove from favorites
-      const index = this.favItems.findIndex(
-        (favItem) => favItem.id === item.id
-      );
-      if (index !== -1) {
-        this.favItems.splice(index, 1);
+    async removeFromFav(item) {
+      this.isRemovingFromFav = true;
+      try {
+        await axios.delete(`http://localhost:5000/api/v1/likes/${item.id}`, { withCredentials: true });
+
+        console.log("Item removed from favorites successfully");
+
+        const index = this.favItems.findIndex(
+          (favItem) => favItem.id === item.id && favItem.size === item.size
+        );
+        if (index !== -1) {
+          this.favItems.splice(index, 1);
+        }
+      } catch (error) {
+        console.error("Error removing item from favorites:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+        }
+      } finally {
+        this.isRemovingFromFav = false;
+      }
+    },
+  },
+  getters: {
+    favItemsCount: (state) => state.favItems.length,
+  },
+  persist: piniaPersistConfig("modalStore"),
+});
+
+export const useCartStore = defineStore("cart", {
+  state: () => ({
+    cartItems: [],
+    isOrdering: false,
+    isAddingToCart: false,
+    isFetchingCart: false,  
+    isRemovingFromCart: false, 
+  }),
+  actions: {
+    async fetchCartItems() {
+      this.isFetchingCart = true; // Set the flag to indicate loading state
+      try {
+        // Send GET request to fetch cart items from the server
+        const response = await axios.get("http://localhost:5000/api/v1/cart",  { withCredentials: true });
+
+        console.log("Cart items fetched successfully:", response.data);
+
+        // Ensure that fetched data is an array before assigning
+        this.cartItems = Array.isArray(response.data.items) ? response.data.items : [];
+      } catch (error) {
+        console.error("Error fetching cart items:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+          console.log("Response headers:", error.response.headers);
+        }
+      } finally {
+        this.isFetchingCart = false; // Reset the loading state
+      }
+    },
+
+    async order() {
+      this.isOrdering = true;
+      try {
+        const response = await axios.post("http://localhost:5000/api/v1/orders", {
+          items: this.cartItems,
+        }, { withCredentials: true });
+
+        // Process response if needed
+        console.log("Order placed successfully:", response.data);
+
+        // Clear cart or handle as per your logic
+        this.cartItems = [];
+      } catch (error) {
+        console.error("Error placing order:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+          console.log("Response headers:", error.response.headers);
+        }
+      } finally {
+        this.isOrdering = false;
+      }
+    },
+
+    async addToCart(item) {
+      this.isAddingToCart = true; // Set the flag to indicate loading state
+      try {
+        // Send POST request to add the item to the server cart
+        const response = await axios.post("http://localhost:5000/api/v1/cart", item,{ withCredentials: true });
+
+        console.log("Item added to server cart successfully:", response.data);
+
+        // Check if item is already in local cartItems
+        const index = this.cartItems.findIndex(
+          (cartItem) => cartItem.id === item.id && cartItem.size === item.size
+        );
+
+        if (index !== -1) {
+          // Item exists in local cartItems, update quantity
+          this.cartItems[index].quantity += 1;
+        } else {
+          // Item does not exist, add new item with quantity 1
+          this.cartItems.push({ ...item, quantity: 1 });
+        }
+      } catch (error) {
+        console.error("Error adding item to cart:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+          console.log("Response headers:", error.response.headers);
+        }
+      } finally {
+        this.isAddingToCart = false; // Reset the loading state
+      }
+    },
+
+    async removeCartItem(itemId) {
+      this.isRemovingFromCart = true; 
+      try {
+        // Send DELETE request to the backend to remove the item
+        await axios.delete(`http://localhost:5000/api/v1/cart/${itemId}`, { withCredentials: true });
+
+        // Update local state to reflect item removal
+        this.cartItems = this.cartItems.filter(item => item.id !== itemId);
+      } catch (error) {
+        console.error("Error removing item from cart:", error);
+        if (error.response) {
+          console.error("Error response data:", error.response.data);
+        }
+      } finally {
+        this.isRemovingFromCart = false;
       }
     },
   },
   getters: {
     totalItems: (state) =>
       state.cartItems.reduce((acc, item) => acc + item.quantity, 0),
+    totalPrice: (state) =>
+      state.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0),
   },
-  persist: piniaPersistConfig('modalStore'),
 });
