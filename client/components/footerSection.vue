593814db2258b565260ce2aab542c275ea865080
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-09-21 19:15:14 +0300

    footer section styling

--- client/components/footerSection.vue
+++ client/components/footerSection.vue
@@ -1,38 +1,33 @@
 <template>
-    <div class="w-full bg-black text-gray-100 tablet:bg-white 
-    tablet:text-black desktop:bg-black desktop:text-white">
-
-        
-
-        <div class=" flex flex-col flex-nowrap gap-4 container mx-auto
-        max-w-4xl h-full justify-between items-center py-6">
-
+    <div class="footer-container">
+        <div class="footer-content container mx-auto max-w-4xl h-full flex flex-col flex-nowrap gap-4 justify-between items-center py-6">
             <div class="footerlinks text-2xl">Join Our Newsletter</div>
             <!--social media links-->
-            <div class="pt-4 tablet:p-0 text-center">
-
-                <a href="#instagram" class="social"><font-awesome-icon class="fa-brands fa-instagram" /></a>
-                <a href="#facebook" class="social"><font-awesome-icon class="fa-brands fa-facebook"  /></a>
-                <a href="#twitter" class="social"><font-awesome-icon class="fa-brands fa-twitter" /></a>
-                <a href="#pinterest" class="social"><font-awesome-icon class="fa-brands fa-pinterest" /></a>
-        
-            </div>
-
-            <div class="footerlinks text-xl text-center ">Do Not Sell My Personal Information</div>         
-            <div class="footerlinks text-lg ">Privacy and Cookies Policy</div>
-            <div class="footerlinks text-base ">Terms & Conditions</div>
+      <div class="social-links">
+        <a href="#instagram" class="social">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/></svg>
+        </a>
+        <a href="#facebook" class="social">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978c.401 0 .955.042 1.468.103a9 9 0 0 1 1.141.195v3.325a9 9 0 0 0-.653-.036a27 27 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.7 1.7 0 0 0-.679.622c-.258.42-.374.995-.374 1.752v1.297h3.919l-.386 2.103l-.287 1.564h-3.246v8.245C19.396 23.238 24 18.179 24 12.044c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.628 3.874 10.35 9.101 11.647"/></svg>
+        </a>
+        <a href="#twitter" class="social">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M41.5 41.5h-7.368L6.5 6.5h7.368l27.632 35zM21.941 26.059L6.5 41.5m35-35L26.059 21.941"/></svg>
+        </a>
+        <a href="#pinterest" class="social">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M9.04 21.54c.96.29 1.93.46 2.96.46a10 10 0 0 0 10-10A10 10 0 0 0 12 2A10 10 0 0 0 2 12c0 4.25 2.67 7.9 6.44 9.34c-.09-.78-.18-2.07 0-2.96l1.15-4.94s-.29-.58-.29-1.5c0-1.38.86-2.41 1.84-2.41c.86 0 1.26.63 1.26 1.44c0 .86-.57 2.09-.86 3.27c-.17.98.52 1.84 1.52 1.84c1.78 0 3.16-1.9 3.16-4.58c0-2.4-1.72-4.04-4.19-4.04c-2.82 0-4.48 2.1-4.48 4.31c0 .86.28 1.73.74 2.3c.09.06.09.14.06.29l-.29 1.09c0 .17-.11.23-.28.11c-1.28-.56-2.02-2.38-2.02-3.85c0-3.16 2.24-6.03 6.56-6.03c3.44 0 6.12 2.47 6.12 5.75c0 3.44-2.13 6.2-5.18 6.2c-.97 0-1.92-.52-2.26-1.13l-.67 2.37c-.23.86-.86 2.01-1.29 2.7z"/></svg>
+        </a>
+      </div>
+            <div class="footerlinks text-xl text-center">Do Not Sell My Personal Information</div>         
+            <div class="footerlinks text-lg">Privacy and Cookies Policy</div>
+            <div class="footerlinks text-base">Terms & Conditions</div>
         </div>
-        
     </div>
 </template>
 
 <script>
 export default {
     setup () {
-        
-
         return {}
     }
 }
 </script>
-
